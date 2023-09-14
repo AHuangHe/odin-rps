80c561ea2c5e59a5e6019404dd2f0cae1b6c780e
Alberto Huang He <aahuanghe@gmail.com>
2023-09-13 21:12:03 -0700

    ah - Add function to play one round

--- rps.js
+++ rps.js
@@ -1,6 +1,38 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+const msgs = ['You won! ', "It's a tie! You both chose ", 'You lost :( '];
 
 let getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
+}
+
+let rpsRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'Rock'){
+        if (computerSelection === 'Scissors') {
+            return msgs[0] + playerSelection + ' beats ' + computerSelection;
+        } else if (computerSelection === 'Rock') {
+            return msgs[1] + playerSelection;
+        } else {
+            return msgs[2] + computerSelection + ' beats ' + playerSelection;
+        }
+    }
+    if (playerSelection === 'Paper'){
+        if (computerSelection === 'Rock') {
+            return msgs[0] + playerSelection + ' beats ' + computerSelection;
+        } else if (computerSelection === 'Paper') {
+            return msgs[1] + playerSelection;
+        } else {
+            return msgs[2] + computerSelection + ' beats ' + playerSelection;
+        }
+    }
+    if (playerSelection === 'Scissors'){
+        if (computerSelection === 'Paper') {
+            return msgs[0] + playerSelection + ' beats ' + computerSelection;
+        } else if (computerSelection === 'Scissors') {
+            return msgs[1] + playerSelection;
+        } else {
+            return msgs[2] + computerSelection + ' beats ' + playerSelection;
+        }
+    }
 }
